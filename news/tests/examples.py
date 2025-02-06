55f3970aacdf555b2fe73d9c78fd9ea1ae1134da
Aleks-Orlov <lexx4432@ya.ru>
2025-02-07 00:44:03 +0300

    Add tests routes and content

--- news/tests/examples.py
+++ news/tests/examples.py
@@ -35,3 +35,51 @@ class TestNews(TestCase):
         # Чтобы проверить равенство с константой -
         # обращаемся к ней через self, а не через cls:
         self.assertEqual(self.news.title, self.TITLE)
+
+
+"""
+При тестировании можно создать несколько клиентов: в одном можно 
+авторизоваться, а из другого клиента работать без авторизации, тестируя 
+сценарии для анонимных пользователей. При этом в каждом тестирующем классе 
+будет доступен и клиент, созданный автоматически.
+"""
+# Импортируем функцию для определения модели пользователя.
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+# Получаем модель пользователя.
+
+User = get_user_model()
+
+
+class TestNews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаём пользователя.
+        cls.user = User.objects.create(username='testUser')
+        # Создаём объект клиента.
+        cls.user_client = Client()
+        # "Логинимся" в клиенте при помощи метода force_login().
+        cls.user_client.force_login(cls.user)
+        # Теперь через этот клиент можно отправлять запросы
+        # от имени пользователя с логином "testUser".
+
+"""
+В ответ на любой запрос, отправленный через клиент, возвращается специальный 
+объект класса Response. В нём содержится ответ сервера и дополнительная 
+информация. Стоит обратить внимание на следующие атрибуты:
+response.status_code — содержит код ответа запрошенного адреса;
+response.content — данные ответа в виде строки байтов;
+response.context — словарь переменных, переданный для отрисовки шаблона при 
+вызове функции render();
+response.templates — перечень шаблонов, вызванных для отрисовки запрошенной 
+страницы;
+"""
+class TestRoutes(TestCase):
+
+    def test_home_page(self):
+        # Вызываем метод get для клиента (self.client)
+        # и загружаем главную страницу.
+        response = self.client.get('/')
+        # Проверяем, что код ответа равен 200.
+        self.assertEqual(response.status_code, 200)
